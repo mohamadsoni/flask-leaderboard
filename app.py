6100ff5c8f54d345764ec726bf5005fd220dd8bb
mohamadsoni <70819698+mohamadsoni@users.noreply.github.com>
2025-03-05 16:43:25 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -114,3 +114,6 @@ def index():
 # **Menjalankan Flask App**
 if __name__ == '__main__':
     app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)  # Ganti port ke 8080 untuk Railway
